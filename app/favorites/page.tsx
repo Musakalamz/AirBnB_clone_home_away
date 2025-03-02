2683896f7429a8039660552d9827be6d86c95c93
Musakalamz <ogunsolumusa@gmail.com>
2025-03-02 10:05:21 +0100

    Shadcn/UI button component

---
+++ app/favorites/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <h1 className="text-3xl">Favorites Page</h1>;
+}
+
+export default page;
---
+++ app/reviews/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <h1 className="text-3xl">Reviews Page</h1>;
+}
+
+export default page;
