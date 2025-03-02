0d85b56e770946ab46d1f18c0571bfb9bf3d4919
Musakalamz <ogunsolumusa@gmail.com>
2025-03-02 11:06:18 +0100

    Installing more Shadcn/UI components and setting up navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 function HomePage() {
   return (
     <div>
-      <h1 className="text-3xl">Home Away</h1>
-      <Button variant="outline" size="lg"className='capitalize m-8">
-        click me
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
       </Button>
     </div>
   );
