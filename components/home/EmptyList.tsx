2f2da95435bcd87499046144cdbdcba8b7288b39
Musakalamz <ogunsolumusa@gmail.com>
2025-04-11 09:53:53 +0100

    Empty list in the HomePage

--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
-export default function EmptyList() {
-  return <div></div>;
+type EmptyListProps = {
+  heading?: string;
+  message?: string;
+  btnText?: string;
+};
+
+export default function EmptyList({
+  heading = "No items in the list.",
+  message = "Keep exploring our properties.",
+  btnText = "back home",
+}: EmptyListProps) {
+  return (
+    <div className="mt-4">
+      <h2 className="text-xl font-bold ">{heading}</h2>
+      <p className="text-lg">{message}</p>
+      <Button asChild className="mt-4 capitalize" size="lg">
+        <Link href="/">{btnText}</Link>
+      </Button>
+    </div>
+  );
 }
