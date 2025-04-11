847cfa20d80878e5209ac34f93a6745f01744d2f
Musakalamz <ogunsolumusa@gmail.com>
2025-04-11 11:26:00 +0100

    return added to jsx  of PropertiesList

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -6,9 +6,11 @@ export default function PropertiesList({
 }: {
   properties: PropertyCardProps[];
 }) {
-  <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
-    {properties.map((property) => {
-      return <PropertyCard key={property.id} property={property} />;
-    })}
-  </section>;
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
+      {properties.map((property) => {
+        return <PropertyCard key={property.id} property={property} />;
+      })}
+    </section>
+  );
 }
