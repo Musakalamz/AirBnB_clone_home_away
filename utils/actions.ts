d22ea74be084e50c7731017e3311296e5b85c752
Musakalamz <ogunsolumusa@gmail.com>
2025-04-20 16:55:28 +0100

    createBookingAction functionality

--- utils/actions.ts
+++ utils/actions.ts
@@ -12,6 +12,7 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { uploadImage } from "./supabase";
+import { calculateTotals } from "./calculateTotals";
 
 // Helper function to get user
 async function getAuthUser() {
@@ -404,6 +405,41 @@ export async function findExistingReview(userId: string, propertyId: string) {
   });
 }
 
-export async function createBookingAction({}) {
-  return { message: "create booking" };
+export async function createBookingAction(prevState: {
+  propertyId: string;
+  checkIn: Date;
+  checkOut: Date;
+}) {
+  const user = await getAuthUser();
+  const { propertyId, checkIn, checkOut } = prevState;
+
+  const property = await db.property.findUnique({
+    where: { id: propertyId },
+    select: { price: true },
+  });
+  if (!property) {
+    return { message: "Property not found" };
+  }
+
+  const { orderTotal, totalNights } = calculateTotals({
+    checkIn,
+    checkOut,
+    price: property.price,
+  });
+
+  try {
+    const booking = await db.booking.create({
+      data: {
+        checkIn,
+        checkOut,
+        orderTotal,
+        totalNights,
+        profileId: user.id,
+        propertyId,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/bookings");
 }
