c5218587a291d4668456e04d369c8c39137958dc
Musakalamz <ogunsolumusa@gmail.com>
2025-04-24 15:21:27 +0100

    Admin page setup

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,19 @@
-export default function page() {
-  return <div></div>;
+import ChartsContainer from "@/components/admin/ChartsContainer";
+import StatsContainer from "@/components/admin/StatsContainer";
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContainer,
+} from "@/components/admin/Loading";
+import { Suspense } from "react";
+
+export default async function page() {
+  <>
+    <Suspense fallback={<StatsLoadingContainer />}>
+      <StatsContainer />
+    </Suspense>
+
+    <Suspense fallback={<ChartsLoadingContainer />}>
+      <ChartsContainer />
+    </Suspense>
+  </>;
 }
