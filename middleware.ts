dff8a924e10f11ef1317f3cd06f848cadcc3aa83
Musakalamz <ogunsolumusa@gmail.com>
2025-04-24 13:00:56 +0100

    Admin Page - Niddleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,38 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/bookings(.*)",
-  "/checkout(.*)",
-  "/favorites(.*)",
-  "/profile(.*)",
-  "/rentals(.*)",
-  "/reviews(.*)",
-]);
+// when it comes to middleware, all of its files are public, so we setup the protected routes below.
+// const isProtectedRoute = createRouteMatcher([
+//   "/bookings(.*)",
+//   "/checkout(.*)",
+//   "/favorites(.*)",
+//   "/profile(.*)",
+//   "/rentals(.*)",
+//   "/reviews(.*)",
+// ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { NextResponse } from "next/server";
+
+// Technically, it's correct but we can flip this as shown below.
+const isPublicRoute = createRouteMatcher(["/", "/properties(.*)"]);
+
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  console.log(auth().userId);
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
+
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
